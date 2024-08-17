fe0359fc2071dc0a569dbc066230a4f8bcd07119
gibrandevlab <146094525+gibrandevlab@users.noreply.github.com>
2024-08-17 21:30:53 +0700

    Update Navbar.js

--- src/Component/landingpage/Navbar.js
+++ src/Component/landingpage/Navbar.js
@@ -52,7 +52,7 @@ function Navbar() {
           className={`w-full md:w-auto ${isMenuOpen ? 'block' : 'hidden'} md:block`}
           id="navbar-default"
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             <li>
               <a
                 href="#"
@@ -72,7 +72,7 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="#"
+                href="#portfolio"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Portfolio
@@ -80,7 +80,7 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="#"
+                href="#contact"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Contact
